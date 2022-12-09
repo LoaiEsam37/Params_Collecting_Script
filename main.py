cd73fb0045c1f275ac8bfdb36a7de54d78ae68f1
Loai Esam <114489561+LoaiEsam37@users.noreply.github.com>
2022-12-09 11:02:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import re
 import requests
 from bs4 import BeautifulSoup # pip3 install bs4
-
+import datetime
 def main():
     # Inputs >>>>>>>>>>
     InputPath = "./input" # FilePath for Input
     OutputPath = "./output" # FilePath for Output
+    Write_To_Output = False # to Write the output to a {OutputPath}
     Show_Info = False # to print Infomation about every process
     Show_Urls = True # to print params to console
     Show_Errors = False # to print errors if errors occuried (This Function won`t stop the code from continue)
     # <<<<<<<<<< Inputs
     with open(InputPath, "r") as f:
         urls = re.split("\n", str(f.read()))
-    # if there is an empty line
-    if urls[-1] == "":
-        del urls[-1]
+    del InputPath
     for url in urls:
         try:
             res = requests.get(url)
             soup = BeautifulSoup(res.content, "html.parser")
-            inputs = soup.findAll("input") # Collect all inputs in html
-            a_tags = soup.findAll('a', attrs={'href': re.compile("^http://")}) # Collect all a in html
-            forms = soup.findAll('form') # Collect all forms in html
             # collect inputs
             params = []
-            for input in inputs:
+            for input in soup.findAll("input"): # Collect all inputs in html
                 try:
-                    temp = "?" + input["name"] + "=" + input["value"] + "&"
-                    params.append(temp)
-                except: pass
+                    params.append("?" + input["name"] + "=" + input["value"] + "&")
+                except Exception as e:
+                    if Show_Errors == True:
+                        print(f"[ ! ]", e)
+            if Show_Info == True:
+                inputs = params
             params = "".join(params)[:-1] # Put all params together and ( [:-1] is for delete & from the end of url)
+            if params == "":
+                continue
             # collect hyperlinks
-            links = []
-            for link in a_tags:
-                links.append(link['href'])
+            links = list(map(lambda link: link["href"], soup.findAll('a', attrs={'href': re.compile("^http://")}))) # Collect all a in html
             # collect forms
-            actions = []
-            for form in forms:
-                actions.append(form["action"])
+            actions = list(map(lambda form: form["action"], soup.findAll('form'))) # Collect all forms in html
             # put all together : params => inputs["name", "value"], links => a["href"], actions => forms["action"]
+            # RegEx >>>>>>>>>>
             for action in actions:
                 for l in links:
                     try:
@@ -47,8 +45,11 @@ def main():
                             DNS1 = l + params
                         else:
                             DNS1 = l + "&" + params
-                    except: pass
-                    # RegEx >>>>>>>>>>
+                        if Show_Urls == True:
+                            print(DNS1)
+                    except Exception as e:
+                        if Show_Errors == True:
+                            print(f"[ ! ]", e)
                     try:
                         # l + action + params
                         if l[len(l)-1:] == "/":
@@ -60,14 +61,22 @@ def main():
                                 DNS2 = l[:-1] + action + params
                         else:
                             DNS2 = l + action + params
-                    except: pass
+                        if Show_Urls == True:
+                            print(DNS2)
+                    except Exception as e:
+                        if Show_Errors == True:
+                            print(f"[ ! ]", e)
                     try:
                         # res.url + params
                         if res.url[len(res.url)-1:] == "/" or l[len(l)-1:] == "&":
                             DNS3 = l + params
                         else:
                             DNS3 = l + "&" + params
-                    except: pass
+                        if Show_Urls == True:
+                            print(DNS3)
+                    except Exception as e:
+                        if Show_Errors == True:
+                            print(f"[ ! ]", e)
                     try:
                         # res.url + action + params
                         if res.url[len(res.url)-1:] == "/":
@@ -79,46 +88,41 @@ def main():
                                 DNS4 = res.url[:-1] + action + params
                         else:
                             DNS4 = res.url + action + params
-                    except: pass
-                    # <<<<<<<<<< RegEx
-                    # Print to Output File
-                    try:
-                        with open(OutputPath, "a") as f:
-                            try:
-                                f.writelines(DNS1)
-                                f.writelines("\n")
-                            except: pass
-                            try:
-                                f.writelines(DNS2)
-                                f.writelines("\n")
-                            except: pass
-                            try:
-                                f.writelines(DNS3)
-                                f.writelines("\n")
-                            except: pass
-                            try:
-                                f.writelines(DNS4)
-                                f.writelines("\n")
-                            except: pass
+                        if Show_Urls == True:
+                            print(DNS4)
                     except Exception as e:
                         if Show_Errors == True:
                             print(f"[ ! ]", e)
-                        else:
-                            pass
+            # <<<<<<<<<< RegEx
+                    # Print Section >>>>>>>>>>
+                    if Write_To_Output == True:
+                        try:
+                            with open(OutputPath, "a") as f:
+                                try:
+                                    f.writelines(f"{DNS1}\n")
+                                except: pass
+                                try:
+                                    f.writelines(f"{DNS2}\n")
+                                except: pass
+                                try:
+                                    f.writelines(f"{DNS3}\n")
+                                except: pass
+                                try:
+                                    f.writelines(f"{DNS4}\n")
+                                except: pass
+                        except Exception as e:
+                            if Show_Errors == True:
+                                print(f"[ ! ]", e)
                     if Show_Info == True:
-                        print("#"*20)
-                        print("[ OK ]", len(inputs), "Inputs detected")
-                        print("[ OK ]", len(links), "hyperlinks detected")
-                        print("[ OK ]", len(actions), "forms actions detected")
-                        print("#"*20)
-                    if Show_Urls == True:
-                        print(DNS1)
-                        print(DNS2)
-                        print(DNS3)
-                        print(DNS4)
+                        print(
+                        "\n####################\n"+
+                        "[ OK ]", len(inputs), "Inputs detected\n"+
+                        "[ OK ]", len(links), "hyperlinks detected\n"+
+                        "[ OK ]", len(actions), "forms actions detected\n"+
+                        "####################\n"
+                        )
         except Exception as e:
             if Show_Errors == True:
                 print(f"[ ! ]", e)
-            else:
-                pass
+                    # <<<<<<<<<< Print Section
 main()
